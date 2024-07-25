ab03d4c7601259dfaaf0695469e8d9d3bcc5ce8a
Davide Di Marco <davide.dimarco5@studio.unibo.it>
2024-07-25 11:33:25 +0200

    start understanding the Environment Grid creation

--- flyPizza/src/main/java/env/FlyPizzaEnv.java
+++ flyPizza/src/main/java/env/FlyPizzaEnv.java
@@ -1,33 +1,48 @@
 package env;
 
-import jason.NoValueException;
 import jason.asSyntax.Literal;
 import jason.asSyntax.NumberTerm;
 import jason.asSyntax.Structure;
 import jason.environment.Environment;
 import jason.environment.grid.Location;
-
 import java.util.logging.Logger;
 
-
-
+/**
+ * Any Jason environment "entry point" should extend
+ * jason.environment.Environment class to override methods init(),
+ * updatePercepts() and executeAction().
+ */
 public class FlyPizzaEnv extends Environment {
+
+   
+
+    static Logger logger = Logger.getLogger(FlyPizzaEnv.class.getName());
+
+    FlyPizzaModel model; // the model of the grid
 
     @Override
     public void init(final String[] args) {
-       System.out.println("Ok, FlyPizza Environment ready to start! 🍕✈️");
+        this.model = new FlyPizzaModel();
+
+        if ((args.length == 1) && args[0].equals("gui")) {
+            final FlyPizzaView view = new FlyPizzaView(this.model);
+            this.model.setView(view);
+        }
+   
+        this.updatePercepts();
     }
-
-
-    void updatePercepts(String ag) {
 
     
+    void updatePercepts() {
+       
+
+      
     }
 
+    
     @Override
     public boolean executeAction(final String ag, final Structure action) {
-       return false;
+       
+        return false;
     }
-
-
 }
---
+++ flyPizza/src/main/java/env/FlyPizzaView.java
@@ -0,0 +1,85 @@
+package env;
+
+import jason.environment.grid.GridWorldView;
+import jason.environment.grid.Location;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.SwingUtilities;
+
+/**
+ * Jason provides a convenient GridWorldView class representing the view of a
+ * square environment consisting of a grid of tiles. Less conveniently, the
+ * Javadoc is almost useless thus you should figure out by yourself (e.g. by
+ * looking at comments in examples source code) how the things work.
+ */
+public class FlyPizzaView extends GridWorldView {
+
+    FlyPizzaModel hmodel;
+
+    public FlyPizzaView(final FlyPizzaModel model) {
+        super(model, "FlyPizza", 700);
+        this.hmodel = model;
+        this.defaultFont = new Font("Arial", Font.BOLD, 16); // change default font
+        SwingUtilities.invokeLater(() -> {
+            this.setVisible(true);
+            this.repaint();
+        });
+    }
+
+    private static final Location copyOf(final Location l) {
+        return new Location(l.x, l.y);
+    }
+
+    /** draw application objects */
+    @Override
+    public void draw(final Graphics g, final int x, final int y, final int object) {
+        final Location lRobot = copyOf(this.hmodel.getAgPos(0));
+        final Location lFridge = copyOf(this.hmodel.lFridge);
+        final Location lOwner = copyOf(this.hmodel.lOwner);
+        
+        final int availableBeers = this.hmodel.availableBeers;
+        final int sipCount = this.hmodel.sipCount;
+        SwingUtilities.invokeLater(() -> {
+            super.drawAgent(g, x, y, Color.lightGray, -1);
+            switch (object) {
+                case FlyPizzaModel.FRIDGE:
+                    if (lRobot.equals(lFridge)) {
+                        super.drawAgent(g, x, y, Color.yellow, -1);
+                    }
+                    g.setColor(Color.black);
+                    this.drawString(g, x, y, this.defaultFont, "Fridge (" + availableBeers + ")");
+                    break;
+                case FlyPizzaModel.OWNER:
+                    if (lRobot.equals(lOwner)) {
+                        super.drawAgent(g, x, y, Color.yellow, -1);
+                    }
+                    String o = "Owner";
+                    if (sipCount > 0) {
+                        o += " (" + sipCount + ")";
+                    }
+                    g.setColor(Color.black);
+                    this.drawString(g, x, y, this.defaultFont, o);
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
+    @Override
+    public void drawAgent(final Graphics g, final int x, final int y, Color c, final int id) {
+        final Location lRobot = copyOf(this.hmodel.getAgPos(0));
+        final Location lFridge = copyOf(this.hmodel.lFridge);
+        final Location lOwner = copyOf(this.hmodel.lOwner);
+        final boolean carryingBeer = this.hmodel.carryingBeer;
+        SwingUtilities.invokeLater(() -> {
+            if (!lRobot.equals(lOwner) && !lRobot.equals(lFridge)) {
+                super.drawAgent(g, x, y, carryingBeer ? Color.orange : Color.yellow, -1);
+                g.setColor(Color.black);
+                super.drawString(g, x, y, this.defaultFont, "Robot");
+            }
+        });
+    }
+}
+
